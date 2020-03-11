b72821ad034dee250c02cee76b955af11ff5ea15
nnadnad <valent.14021999@gmail.com>
2020-03-11 17:08:56 +0700

    add layout homescreen

--- app/src/main/java/com/example/bolasepak/HomeActivity.java
+++ app/src/main/java/com/example/bolasepak/HomeActivity.java
@@ -1,6 +1,16 @@
 package com.example.bolasepak;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class HomeActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+
+    }
 }
