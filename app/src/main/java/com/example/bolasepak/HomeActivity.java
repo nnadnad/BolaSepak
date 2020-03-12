3c896dbd432a2b5bba92e01659b8ac9b669e3e27
nnadnad <valent.14021999@gmail.com>
2020-03-12 10:02:19 +0700

    parsing json event

--- app/src/main/java/com/example/bolasepak/HomeActivity.java
+++ app/src/main/java/com/example/bolasepak/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.bolasepak;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -29,9 +30,15 @@ public class HomeActivity extends AppCompatActivity {
     HashMap<String,String> hashMapUrl;
     ProgressBar progressBar;
 
+    //Untuk databse SQLLite
+    SQLiteManager sqLiteManager ;
+    SharedPreferences preferences ;
 
-    String url_next_15_events = "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328";
-    String url_past_15_events = "https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=4328";
+    String BASE_URL = "https://www.thesportsdb.com/api/v1/json/1";
+
+
+    String url_past_15_events = BASE_URL + "/eventspastleague.php?id=4328";
+    String url_next_15_events = BASE_URL + "/eventsnextleague.php?id=4328";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,18 +70,22 @@ public class HomeActivity extends AppCompatActivity {
                         String id_away_team = jsonObject.getString("id_away_team") ;
                         String nama_home_team = jsonObject.getString("strHomeTeam") ;
                         String nama_away_team = jsonObject.getString("strAwayTeam") ;
-                        String score_home_team = jsonObject.getString("intHomeScore") ;
-                        String score_away_team = jsonObject.getString("intAwayScore") ;
+                        String score_home_team = jsonObject.getString("score_home_team") ;
+                        String score_away_team = jsonObject.getString("score_away_team") ;
                         String logo_home_team = hashMapUrl.get(id_home_team) ;
                         String logo_away_team = hashMapUrl.get(id_away_team) ;
-
-                        String shots_home_team = jsonObject.getString("intHomeShots") ;
-                        String shots_away_team = jsonObject.getString("intAwayShots") ;
+                        String shots_home_team = jsonObject.getString("shots_home_team") ;
+                        String shots_away_team = jsonObject.getString("shots_away_team") ;
                         String goals_home_team = jsonObject.getString("strHomeGoalDetails") ;
                         String goals_away_team = jsonObject.getString("strAwayGoalDetails") ;
+                        String matchDate = jsonObject.getString("match_date") ;
+                        String matchTime = jsonObject.getString("strTime") ;
 
-                        String match_date = jsonObject.getString("dateEvent") ;
-                        String match_time = jsonObject.getString("strTime") ;
+                        MatchData matchData = new MatchData();
+                        matchData.setId_match(id_match);
+                        matchData.setId_home_team(id_home_team);
+                        matchData.setId_away_team(id_away_team);
+                        matchData.setNama_home_team(nama_home_team);
 
                     }
                 } catch (Exception e) {
@@ -87,5 +98,9 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void getEventNextData() {
+
     }
 }
