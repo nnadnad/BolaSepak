3b7538f53ac7c587206bddd7a42636bcf735b6b4
nnadnad <valent.14021999@gmail.com>
2020-03-13 11:15:40 +0700

    fix layout

--- app/src/main/java/android/example/pbd_bolasepak/MainActivity.java
+++ app/src/main/java/android/example/pbd_bolasepak/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_screen);
         listTeams = new HashMap<String, String>();
         listMatch = new ArrayList<MatchDetails>();
 
